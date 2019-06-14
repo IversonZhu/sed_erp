204218faf9d53e22ae6a788084ff19c758b30c01
Iverson <zhuhuayao_2014@163.com>
2019-06-14 17:35:56 +0800

    新增商品基类

---
+++ src/main/java/com/iverson/sed_erp/pojo/Goods.java
@@ -0,0 +1,31 @@
+package com.iverson.sed_erp.pojo;
+
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+public class Goods {
+    /** id */
+    private int id;
+    /** 商品编号 */
+    private String goodsNo;
+    /** 商品名称 */
+    private String name;
+    /** 商品条码 */
+    private String barcode;
+    /** 商品重量 */
+    private Double weight;
+    /** 商品价格 */
+    private Double price;
+    /** 分类编号 */
+    private String categoryNo;
+    /** 品牌编号 */
+    private String brandNo;
+    /** 库存 */
+    private Integer store;
+    /** 创建时间 */
+    private Date createTime;
+    /** 修改时间 */
+    private Date modifyTime;
+}
