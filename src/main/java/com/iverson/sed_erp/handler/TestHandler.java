516422976c92c08f5e2d7999ab2518cf2d705888
Iverson <zhuhuayao_2014@163.com>
2019-06-27 17:34:31 +0800

    添加部门数据操作功能

---
+++ src/main/java/com/iverson/sed_erp/handler/TestHandler.java
@@ -0,0 +1,5 @@
+package com.iverson.sed_erp.handler;
+
+public class TestHandler {
+    //这个是提供给设备是云调用的
+}
--- src/main/java/com/iverson/sed_erp/mapper/DepartmentMapper.java
+++ src/main/java/com/iverson/sed_erp/mapper/DepartmentMapper.java
@@ -3,7 +3,11 @@ package com.iverson.sed_erp.mapper;
 import com.iverson.sed_erp.pojo.Department;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 @Mapper
@@ -11,4 +15,10 @@ public interface DepartmentMapper {
 
     @Insert("insert into (department_no,name,status) values (#{departmentNo},#{name},#{status}")
     int addOne(Department department);
+
+    @Update("")
+    int update();
+
+    @Select("")
+    List<Department> getList();
 }
